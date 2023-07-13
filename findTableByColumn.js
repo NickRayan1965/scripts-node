ff2b7e5b239cdce73f930f8a963dfc732c1b2a24
NickRayan <nickcerron@gmail.com>
2023-07-13 12:39:16 -0500

    FindTableByColumn

---
+++ findTableByColumn.js
@@ -0,0 +1,52 @@
+const config = {
+  user: 'sa',
+  password: '1234',
+  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance,
+  port: 1433,
+  //activate trustServerCertificate: true
+  // If you're on Windows Azure, you will need this:
+  trustServerCertificate: true,
+};
+const mssql = require('mssql');
+const findTableByColumn = async ({
+  column,
+  database,
+  strictSearch = true,
+  notLikeExclutions = [],
+}) => {
+  const pool = new mssql.ConnectionPool({ ...config, database });
+  await pool.connect();
+  //imprimir todas las tablas
+  const likeExpression = strictSearch
+    ? `like '${column}'`
+    : `like '%${column}%'`;
+  const notLikeExclutionExpresions = notLikeExclutions
+    .map((e) => `and TABLE_NAME not like '%${e}%'`)
+    .join(' ');
+  console.log(notLikeExclutionExpresions);
+  const result = await pool
+    .request()
+    .query(
+      `SELECT TABLE_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'dbo' and COLUMN_NAME ${likeExpression} ${
+        notLikeExclutions.length > 0 ? notLikeExclutionExpresions : ''
+      } GROUP BY TABLE_NAME `
+    );
+  const tableNames = result.recordset.map((r) => r.TABLE_NAME);
+  return { [database]: tableNames };
+};
+
+(async () => {
+  const databases = ['BACKOFFICESETUP', 'BACKOFFICE'];
+  const column = 'cdlocal';
+  const results = await Promise.all(
+    databases.map((database) =>
+      findTableByColumn({
+        column,
+        database,
+        strictSearch: true,
+        notLikeExclutions: ['Afilia', 'Anoma', 'cajar', 'DiaContom', 'Cierre_M', 'NCredito', 'NDebito',],
+      })
+    )
+  );
+  console.log(results);
+})();
